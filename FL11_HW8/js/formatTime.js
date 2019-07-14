bb108d391d8f52d85c6bb956ad19d7dfb8e739e0
Taras Sidak <tarasidak1990@gmail.com>
2019-07-14 23:14:05 +0300

    Delete formatTime.js

--- FL11_HW8/js/formatTime.js
+++
@@ -1,9 +0,0 @@
-const formatTime = time => {
-    let days = Math.floor(time / 1440);
-    let hours = Math.floor(time / 60) % 24;
-    let minutes = time % 60;
-    console.log(days + ' day(s) ' + hours + ' hour(s) ' + minutes + ' minutes(s)');
-}
-formatTime(120);
-formatTime(59);
-formatTime(3601);
